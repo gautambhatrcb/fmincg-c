e42be1b7147bd90a6f4057fc98d0ced40ba20da2
Sagar G V <sagar.writeme@gmail.com>
2013-07-29 19:56:10 +0530

    Example cost function changed

--- cpu/example.c
+++ cpu/example.c
@@ -3,19 +3,19 @@
 
 void costFunc(float* xVec,float* cost,float* gradVec)
 {
-	// f(x,y) = (x-10)^2 + (y-4)^2
+	// f(x,y) = (x-10)^4 + (y-4)^2
 	
-	*cost = (xVec[0]-10)*(xVec[0]-10) + (xVec[1]-4)*(xVec[1]-4);
+	*cost = (xVec[0]-10)*(xVec[0]-10)*(xVec[0]-10)*(xVec[0]-10) + (xVec[1]-4)*(xVec[1]-4);
 	
 	//printf("x = (%f,%f) , C = %f\n",xVec[0],xVec[1],*cost);
 	
-	gradVec[0] = 2*(xVec[0]-10);
+	gradVec[0] = 4*(xVec[0]-10)*(xVec[0]-10)*(xVec[0]-10);
 	gradVec[1] = 2*(xVec[1]-4);
 }
 
 int main()
 {
-	float x[] = { 0,0 };
+	float x[] = { 0,0 }; // initial guess
 	
 	int ret = fmincg(&costFunc, x, 2, 100); // nDim = 2 , maxCostFunctionCalls = 100
 	
